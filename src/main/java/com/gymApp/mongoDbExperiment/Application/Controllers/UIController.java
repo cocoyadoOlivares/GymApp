4758ead07e6093a1fa3afbaed88d784a74919eb7
Jorge Olivares <jorge.olivares.garza@gmail.com>
2018-05-10 14:45:05 -0500

    initial commit

---
+++ src/main/java/com/gymApp/mongoDbExperiment/Application/Controllers/UIController.java
@@ -0,0 +1,13 @@
+package com.gymApp.mongoDbExperiment.Application.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UIController {
+
+    @RequestMapping(value ="/")
+    public String index(){
+        return "index";
+    }
+}
